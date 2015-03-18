f4ddb14558fefd46e909bc9ea566ab731f1110ff
Florian Lier <flier@techfak.uni-bielefeld.de>
2015-03-18 12:09:51 +0100

    Fixed sed_host script

--- unittesting/sed_host_paths.sh
+++ unittesting/sed_host_paths.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-for i in `ls |grep ssh`; do sed -i "s/<execution_host val=\"localhost\" \/>/<execution_host val=\"\${HOSTNAME}\" \/>/" $i; done
+for i in `ls |grep ssh`; do sed -i "s/<execution_host val=\"localhost\" \/>/<execution_host val=\"\$HOSTNAME\" \/>/" $i; done
 for i in `ls |grep ssh`; do sed -i "s/<path val=\"\/homes\/flier\/dev\/fsmt\/fsmt-0.18\/bin\/\" \/>/<path val=\"\$WORKSPACE\/bin\/\" \/>/" $i; done
