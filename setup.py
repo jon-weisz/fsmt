fdd9c8dd71a8291e94dcc7e83bb106d031d63372
Florian Lier <flier@techfak.uni-bielefeld.de>
2015-07-30 13:35:32 +0200

    Fixed PyLint dependency for logilab-common

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ setup(name="fsmtest",
 
       install_requires=['Louie', 'termcolor', 'eventlet', 'suds',
                         'restlib', 'lxml', 'nose==1.2.1', 'coverage',
-                        'nosexcover', 'pylint', 'setuptools-lint',
+                        'nosexcover', 'pylint=1.4.4', 'setuptools-lint',
                         'psutil', 'sphinx_bootstrap_theme',
                         'setuptools-pep8'])
 
